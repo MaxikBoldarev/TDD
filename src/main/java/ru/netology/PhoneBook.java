cc9d0d4ad1501ce1328c6dd41840eaa013721ece
Maxim Boldarev <maxikboldarev13@yandex.ru>
2023-09-21 12:44:37 +0200

    add implemented

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -7,6 +7,11 @@ public class PhoneBook {
     private static Map<String,String> phoneBook = new HashMap<>();
 
     public int add(String name,String number){
-        return 0;
+        if(!phoneBook.containsKey(name)){
+            phoneBook.put(name,number);
+        } else {
+            phoneBook.replace(name,phoneBook.get(name),number);
+        }
+        return phoneBook.size();
     }
 }
