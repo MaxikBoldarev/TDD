24ef2e2686a3b4ecb2acdff467f252e3d51c869d
Maxim Boldarev <maxikboldarev13@yandex.ru>
2023-09-21 12:34:52 +0200

    Stub and test for add

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,4 +1,12 @@
 package ru.netology;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private static Map<String,String> phoneBook = new HashMap<>();
+
+    public int add(String name,String number){
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,20 @@
+
+import org.junit.jupiter.api.Test;
+import ru.netology.PhoneBook;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhoneBookTest {
+    private static PhoneBook phoneBook = new PhoneBook();
+
+    @Test
+    public void addTest() {
+        int expected = 2;
+        phoneBook.add("Oleg","8950111");
+        phoneBook.add("Ira","8951240");
+        int actual = phoneBook.add("Oleg","8908754");
+        assertEquals(expected,actual);
+    }
+
+
 }
